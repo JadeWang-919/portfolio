847b80e0c90f9e369b4a2b99645261a60dcfea5c
JadeWang-919 <angelawangqi@hotmail.com>
2022-08-27 19:57:40 -0400

    first commit

---
+++ js/script.js
@@ -0,0 +1,22 @@
+///////////////////////////////////////////////////////////
+// Set current year
+const yearEl = document.querySelector(".year");
+const currentYear = new Date().getFullYear();
+yearEl.textContent = currentYear;
+
+///////////////////////////////////////////////////////////
+// Make mobile navigation work
+
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+
+btnNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+});
+
+
+const navLink = document.querySelector(".main-nav-link");
+
+navLink.addEventListener("click", function () {
+  headerEl.classList.remove("nav-open");
+});
